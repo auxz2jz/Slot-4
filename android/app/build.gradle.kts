60d2228b818746af091b42d20ca64587be861c9b
auxz2jz <41704988+auxz2jz@users.noreply.github.com>
2026-09-17 02:56:33 -0700

    Compile against Android 17 while targeting API 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.zaksecurity.construction3dbuilder"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.zaksecurity.construction3dbuilder"
